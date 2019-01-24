6f8d607075a4454ed5c713ffcb69ab7a262b79d4
Alonso Ortega <alonsojortega@gmail.com>
2019-01-24 22:39:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,4 +24,13 @@ def assign_rooms(attendees)
   end
 
   room_assignments
+
+  # attendees.each_with_index.map do |attendee, index|
+  #   "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  # end
+
+end
+
+def printer(attendee)
+  
 end
