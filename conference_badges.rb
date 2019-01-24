0d2364ef22323e48578c7c462664d84963f5ce64
Alonso Ortega <alonsojortega@gmail.com>
2019-01-24 22:27:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  list = []
+
+  attendees.each do |attendee|
+    list.push("Hello, my name is #{attendee}.")
+  end
+
+  list
+end
